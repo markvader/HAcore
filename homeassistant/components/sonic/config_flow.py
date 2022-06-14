28939433bf651674a809ec54253fd97e530c509b
markvader <markjbreen@gmail.com>
2022-06-14 23:38:57 +0100

    integration listed in dev environment, but reporting "Error occurred loading configuration flow for integration sonic: No module named 'sonic'"

--- homeassistant/components/sonic/config_flow.py
+++ homeassistant/components/sonic/config_flow.py
@@ -11,7 +11,7 @@ from .const import DOMAIN, LOGGER
 # DATA_SCHEMA = vol.Schema({vol.Required(CONF_USERNAME): str, vol.Required(CONF_PASSWORD): str})
 DATA_SCHEMA = vol.Schema({vol.Required("username"): str, vol.Required("password"): str})
 
-async def validate_input(hass: core.HomeAssistant, data):
+async def validate_input(hass: HomeAssistant, data):
     """Validate the user input allows us to connect.
 
     Data has the keys from DATA_SCHEMA with values provided by the user.
@@ -41,13 +41,17 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         errors = {}
         if user_input is not None:
-            await self.async_set_unique_id(user_input[CONF_USERNAME])
-            self._abort_if_unique_id_configured()
             try:
                 info = await validate_input(self.hass, user_input)
+
                 return self.async_create_entry(title=info["title"], data=user_input)
             except CannotConnect:
                 errors["base"] = "cannot_connect"
+            except InvalidHost:
+                errors["host"] = "cannot_connect"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
 
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
@@ -56,3 +60,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
 class CannotConnect(exceptions.HomeAssistantError):
     """Error to indicate we cannot connect."""
+
+
+class InvalidHost(exceptions.HomeAssistantError):
+    """Error to indicate there is an invalid hostname."""
