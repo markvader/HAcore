232738dc2f8901de4cc508e35c4e4e86d0920e2c
markvader <markjbreen@gmail.com>
2022-06-21 14:39:27 +0100

    add pressure_tests_enabled switch

--- homeassistant/components/sonic/switch.py
+++ homeassistant/components/sonic/switch.py
@@ -2,10 +2,13 @@
 from __future__ import annotations
 
 import voluptuous as vol
+from dataclasses import dataclass
+from typing import Any
 
-from homeassistant.components.switch import SwitchEntity
+from homeassistant.components.switch import SwitchEntity, SwitchEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN as SONIC_DOMAIN
@@ -98,7 +101,7 @@ class AutoShutOffSwitch(PropertyEntity, SwitchEntity):
 
     @property
     def icon(self):
-        """Return the icon to use for the valve."""
+        """Return the icon to use for the switch."""
         if self.is_on:
             return "mdi:auto-fix"
         return "mdi:exclamation-thick"
@@ -119,6 +122,49 @@ class AutoShutOffSwitch(PropertyEntity, SwitchEntity):
     def async_update_state(self) -> None:
         """Retrieve the latest switch state and update the state machine."""
         self._state = self._device.property_auto_shut_off == True
+        self.async_write_ha_state()
+
+    async def async_added_to_hass(self):
+        """When entity is added to hass."""
+        self.async_on_remove(self._device.async_add_listener(self.async_update_state))
+
+
+class PressureTestsEnabled(PropertyEntity, SwitchEntity):
+    """Switch class for the Property pressure_tests_enabled."""
+
+    def __init__(self, device: PropertyDataUpdateCoordinator) -> None:
+        """Initialize the Property Pressure Tests Enabled switch."""
+        super().__init__("pressure_tests_enabled", "Pressure Tests Function", device)
+        self._state = self._device.property_pressure_tests_enabled == True
+
+    @property
+    def is_on(self) -> bool:
+        """Return True if the Pressure Tests Enabled is enabled."""
+        return self._state
+
+    @property
+    def icon(self):
+        """Return the icon to use for the switch."""
+        if self.is_on:
+            return "mdi:auto-fix"
+        return "mdi:exclamation-thick"
+
+    async def async_turn_on(self, **kwargs) -> None:
+        """Turn on the Pressure Tests Enabled Function"""
+        await self._device.api_client.property.async_update_property_settings(self._device.id, json={'pressure_tests_enabled': True})
+        self._state = True
+        self.async_write_ha_state()
+
+    async def async_turn_off(self, **kwargs) -> None:
+        """Turn off the Pressure Tests Enabled Function"""
+        await self._device.api_client.property.async_update_property_settings(self._device.id, json={'pressure_tests_enabled': False})
+        self._state = False
+        self.async_write_ha_state()
+
+    @callback
+    def async_update_state(self) -> None:
+        """Retrieve the latest switch state and update the state machine."""
+        self._state = self._device.property_pressure_tests_enabled == True
         self.async_write_ha_state()
 
     async def async_added_to_hass(self):
