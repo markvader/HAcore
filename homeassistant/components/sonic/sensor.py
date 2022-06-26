6967e3fccfa72301fb880eea08f08937052e4cc7
markvader <markjbreen@gmail.com>
2022-06-26 23:14:21 +0100

    add timezone retrieved from API

--- homeassistant/components/sonic/sensor.py
+++ homeassistant/components/sonic/sensor.py
@@ -157,7 +157,7 @@ class SonicBatterySensor(SonicEntity, SensorEntity):
         return self._device.battery_state
 
 
-class SonicTelemetryTime(SonicEntity, PropertyEntity, SensorEntity):
+class SonicTelemetryTime(PropertyEntity, SensorEntity):
     """Returns time that the telemetry data was captured at by sonic."""
 
     _attr_icon = TIMER_ICON
@@ -173,7 +173,8 @@ class SonicTelemetryTime(SonicEntity, PropertyEntity, SensorEntity):
     def native_value(self) -> str | None:
         """Return the current telemetry time state."""
         telemetry_timestamp = self._device.last_heard_from_time
-        telemetry_datetime = datetime.fromtimestamp(telemetry_timestamp, tz=(self._property.property_timezone))
+        telemetry_timezone = self._device.property_timezone
+        telemetry_datetime = datetime.fromtimestamp(telemetry_timestamp, tz=telemetry_timezone)
         return telemetry_datetime
 #        return self._device.last_heard_from_time
 
